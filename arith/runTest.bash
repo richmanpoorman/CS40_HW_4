c3dd7c48bd27bc5a82b713c5ad152664f6fcfdcc
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-10-16 17:45:09 -0400

    default

--- arith/runTest.bash
+++ arith/runTest.bash
@@ -3,5 +3,5 @@
 IMAGE_PATH="Test/TestImages/"
 IMAGES=("flowers" "animals" "desert" "erosion" "from-wind-cave" \
         "halligan" "in-wind-cave" "rock" "segfault" "wind-cave", "mobo") 
-        
 OUTPUT_PATH="Test/OutputTest/"
+

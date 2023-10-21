fe48e34ba6056a949db25f0abdb179d517296100
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2023-10-20 20:07:44 -0400

    added contracts

--- arith/DCTStep.c
+++ arith/DCTStep.c
@@ -22,7 +22,8 @@ static void dctToCie(int col, int row, A2Methods_UArray2 uarray2,
  *  Output    : (None)
  *  Notes     : Assumes that the Pnm_ppm is in proper format, with the
  *              values being Pnm_rgb structs (no way to check :( );
- *              Will CRE if can not allocate new memory
+ *              Will CRE if can not allocate new memory;
+ *              Will change the width and height of the image
  */
 static void compress(Pnm_ppm image)
 {
@@ -50,6 +51,20 @@ static void compress(Pnm_ppm image)
         image -> height = newHeight;
 }
 
+/*
+ *  Name      : cieToDct
+ *  Purpose   : Put the correct DCT pixel into the image for each pixel
+ *  Parameters: (int)                col     = The column of the current pixel
+ *              (int)                row     = The row of the current pixel
+ *              (A2Methods_UArray2)  uarray2 = The new image pixels
+ *              (A2Methods_Object *) ptr     = The DCT pixel in the new image
+ *              (void *)             cl      = The CIE image to convert from
+ *  Output    : (None)
+ *  Notes     : Assumes that the Pnm_ppm is in proper format, with the
+ *              values being Pnm_rgb structs (no way to check :( );
+ *              Should only be used as a mapping function to the new 
+ *              DCT pixel array
+ */
 static void cieToDct(int col, int row, A2Methods_UArray2 uarray2, 
                      A2Methods_Object *ptr, void *cl)
 {
@@ -61,11 +76,13 @@ static void cieToDct(int col, int row, A2Methods_UArray2 uarray2,
         int oldCol = col * 2;
         int oldRow = row * 2;
 
+        /* Gets the 2x2 pixels values */
         Cie_float topLeft  = methods -> at(pixels, oldCol    , oldRow    );
         Cie_float topRight = methods -> at(pixels, oldCol + 1, oldRow    );
         Cie_float botLeft  = methods -> at(pixels, oldCol    , oldRow + 1);
         Cie_float botRight = methods -> at(pixels, oldCol + 1, oldRow + 1);
 
+        /* Gets the average of the 2x2 block for pb and pr */
         float pbAverage = (
                            topLeft  -> pb +
                            topRight -> pb + 
@@ -79,16 +96,19 @@ static void cieToDct(int col, int row, A2Methods_UArray2 uarray2,
                            botRight -> pr
                           ) / 4;
         
+        /* Gets Y1, Y2, Y3, Y4 */
         float y1 = topLeft  -> y;
         float y2 = topRight -> y;
         float y3 = botLeft  -> y;
         float y4 = botRight -> y;
 
+        /* Converts y -> a, b, c, d for the dct format*/
         float a = (y4 + y3 + y2 + y1) / 4;
         float b = (y4 + y3 - y2 - y1) / 4;
         float c = (y4 - y3 + y2 - y1) / 4;
         float d = (y4 - y3 - y2 + y1) / 4;
 
+        /* Puts the DCT pixel on the stack */
         struct Dct_float newPixel = {
                 clampToRange(a        ,  0.0, 1.0), 
                 clampToRange(b        , -0.5, 0.5), 
@@ -105,13 +125,14 @@ static void cieToDct(int col, int row, A2Methods_UArray2 uarray2,
 
 /*
  *  Name      : decompress
- *  Purpose   : Turns the pixels from Rgb_float to Pnm_rgb
- *              going from RGB float to RGB int
- *  Parameters: (Pnm_ppm) image = The original image convert format
+ *  Purpose   : Turns the pixels from Dct_float to Cie_float
+ *              going from DCT float to CIE int
+ *  Parameters: (Pnm_ppm) image = Image with DCT floats as pixels
  *  Output    : (None)
  *  Notes     : Assumes that the Pnm_ppm is in proper format, with the
  *              values being Rgb_float structs (no way to check :( );
- *              Will CRE if can not allocate new memory
+ *              Will CRE if can not allocate new memory;
+ *              Will change the width and height of the image
  */
 static void decompress(Pnm_ppm image)
 {
@@ -141,6 +162,20 @@ static void decompress(Pnm_ppm image)
         image -> height = newHeight;
 }
 
+/*
+ *  Name      : dctToCie
+ *  Purpose   : Put the correct CIE pixel into the image for each pixel
+ *  Parameters: (int)                col     = The column of the current pixel
+ *              (int)                row     = The row of the current pixel
+ *              (A2Methods_UArray2)  uarray2 = The new image pixels
+ *              (A2Methods_Object *) ptr     = The CIE pixel in the new image
+ *              (void *)             cl      = The DCT image to convert from
+ *  Output    : (None)
+ *  Notes     : Assumes that the Pnm_ppm is in proper format, with the
+ *              values being Pnm_rgb structs (no way to check :( );
+ *              Should only be used as a mapping function to the new 
+ *              CIE pixel array
+ */
 static void dctToCie(int col, int row, A2Methods_UArray2 uarray2, 
                      A2Methods_Object *ptr, void *cl)
 {
--- arith/PixelStructs.c
+++ arith/PixelStructs.c
@@ -1,9 +1,20 @@
+
+
 #include <math.h>
 
 float clampToRange(float input, float lower, float upper);
 int   floatToInt(float input, int denominator, int lower, int upper);
 float intToFloat(int input, int denominator, float lower, float upper);
 
+/*
+ *  Name      : clampToRange
+ *  Purpose   : Takes a float, and forces it within the range
+ *  Parameters: (float) input = The float to bound
+ *              (float) lower = The lower bound of the range
+ *              (float) upper = The upper bound of the range
+ *  Output    : (float) The value of input if it is in the range, 
+ *                      or the corresponding bound if it is out of range
+ */
 float clampToRange(float input, float lower, float upper)
 {
         if (input < lower) {
@@ -15,6 +26,17 @@ float clampToRange(float input, float lower, float upper)
         return input;
 }
 
+/*
+ *  Name      : floatToInt
+ *  Purpose   : Takes a float and turns it into an int, represented 
+ *              approximately by input / denominator
+ *  Parameters: (float) input       = The starting float to scale
+ *              (int)   denominator = The scale factor
+ *              (int)   lower       = The lower range the int can take
+ *              (int)   upper       = The upper range the int can take
+ *  Output    : (int) The value scaled by denominator, put into an 
+ *                    int instead of a float
+ */
 int   floatToInt(float input, int denominator, int lower, int upper)
 {
         
@@ -29,6 +51,17 @@ int   floatToInt(float input, int denominator, int lower, int upper)
         return (int)scaled;
 }
 
+/*
+ *  Name      : intToFloat
+ *  Purpose   : Takes a int and turns it into an float, represented 
+ *              approximately by input / denominator
+ *  Parameters: (int)   input       = The starting int to scale
+ *              (int)   denominator = The scale factor to divide by
+ *              (float) lower       = The lower range the float can take
+ *              (float) upper       = The upper range the float can take
+ *  Output    : (int) The value scaled by denominator, put into an 
+ *                    float instead of (input / denominator)
+ */
 float intToFloat(int input, int denominator, float lower, float upper)
 {
         float value = 1.0 * input / denominator;
